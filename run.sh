b3435993fb5b9911c29cf927cf56941aeb8da478
AB <ab3250@live.com>
2021-05-19 12:24:54 -0400

    calc ws base

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+WD="$(pwd)";sed -i '/.*set \$rtdir.*/c\set $rtdir '"$(pwd)"';' $(pwd)/nginx/conf/nginx.conf
+
+sudo $(pwd)/nginx/sbin/nginx -c $(pwd)/nginx/conf/nginx.conf -p $(pwd)/nginx &
+
+/usr/bin/google-chrome-stable localhost &
+guile start.scm
+
+
+
+#GUILE_LOAD_PATH="$PWD:$GUILE_LOAD_PATH" guile test.scm
+#/media/stor/gentoo-files/execs/compilers/programs/scheme/guile/SSE/upstream/guile-websocket#
+#sudo lighttpd -D -f /etc/lighttpd/lighttpd.conf
