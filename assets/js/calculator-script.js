37a4f670c0786b540f023c4e0b63b36c6ed279c9
AB <ab3250@live.com>
2021-05-25 12:44:01 -0400

    202105251pm

--- assets/js/calculator-script.js
+++ assets/js/calculator-script.js
@@ -1,19 +1,14 @@
 let display
-let ws
-
+let register = ''
+let regexDig=/^dig([0-9]{1}|[p])$/
+let regexOpr=/^opr([a]|[s]|[d]|[m]|[c]|[r]|[e]|[x])$/
+let regexCmd=/^cmd([b])$/
 document.addEventListener('DOMContentLoaded', loadWindow, false)
-
-window.setInterval('animate()', 3000)
-
-function mode (btnID) {	
-      ws.send(btnID)
-}
 
 function loadWindow () {
 	Array.from(document.getElementsByTagName('button')).forEach(function (value, i, col) {
     	 col[i].onclick = function (e) { mode(e.target.id) }
 	})
-    	ws = new WebSocket("ws://localhost:9090")
     	display = new SegmentDisplay("display")
 	display.pattern         = '#.#.#.#.#.#.#.#.#.#.#.#.#.';
     	display.segmentCount    = SegmentDisplay.FourteenSegment
@@ -29,30 +24,37 @@ function loadWindow () {
     	let canvas = document.getElementsByTagName('canvas')[0]
     	canvas.width  = 900
     	canvas.height = 70
-    	ws.onmessage = function(event) {
-   	console.log((JSON.parse(event.data)).data)
-   	displayNumber(numeral((JSON.parse(event.data)).data).format('0.0'))
-	}
-    	ws.onopen = function() {
-		console.log("connected")
-	}
-    	ws.onclose = function() {
-		console.log("closed websocket")
-    	}
-    	ws.onerror = function(event) {
-  		console.log("WebSocket error observed:", event)
-    	}
 }
-function animate() { 
-	let a = new Decimal('-5032485723458348569331745.33434346346912144534543'),
-    	b = new Decimal('5.6700000'),
-    	c = new Decimal('3.5444')
-    	d = a.dividedBy(b).dividedBy(c)
-	formatNumber(a)
-} 
 
-
-
+function mode (btnID) {	
+	let btn = btnID[3]
+	if (regexDig.test(btnID)){
+		switch(btn){
+			case 'p':
+				register.includes(".") ? register += '' : register += '.'
+				break;
+			case '0': 
+				register.length === 0 ? register += '' : register += '0'
+				break;
+			default: register += btn 
+		}
+	}else if (regexOpr.test(btnID)){
+		//switch(btn){
+			//case 'b':				 
+			//	if(register.length !== 0) register = register.substring(0,register.length-1)
+			//	break;
+		//default: register += btn 
+	//	}
+	}else if (regexCmd.test(btnID)) { 
+		switch(btn){
+			case 'b':				 
+				if(register.length !== 0) register = register.substring(0,register.length-1)
+				break;
+			default: register += btn
+		}
+	}
+      	displayNumber(register)
+}
 
 
 
