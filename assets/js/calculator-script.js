f688e78189b81aa6cc8856c6fbb0c4797e27b049
AB <ab3250@live.com>
2021-07-02 21:38:32 -0400

    2021_07_02_09_38_PM

--- assets/js/calculator-script.js
+++ assets/js/calculator-script.js
@@ -9,69 +9,87 @@ const regexSpl=/^spl([c]|[b])$/ //non-mode changing commands backspace chs
 
 function buttonPress (btnID) {
 	let btn = btnID[3]
-	if (regexDig.test(btnID)){
-		dataEntryMode  === false ? (register = '', dataEntryMode = true): null
-		switch(btn){
-			case 'p':
-				register.includes(".") ? register += '' : register.length === 0 ? register += '0.' : register += '.'
-				break
-			case '0':
-				register.length === 0 ? register += '' : register += '0'
-				break
-			default: register += btn
+
+	switch(true){
+		case regexDig.test(btnID) : {
+			dataEntryMode  === false ? (register = '', dataEntryMode = true): null
+			switch(btn){
+				case 'p':
+					register.includes(".") ? register += '' : register.length === 0 ? register += '0.' : register += '.'
+					break
+				case '0':
+					register.length === 0 ? register += '' : register += '0'
+					break
+				default: register += btn
+			}
+			
 		}
-	}else if (regexOpr.test(btnID)){
-		dataEntryMode === true ? (stack.push(format(Number(register))),dataEntryMode = false) : null 
-		switch(btn){
-			case 'g': //x<>y
-				stack.swapxy()
-				register = stack.peek(0)
-				break
-			case 'a': //add
-				calc2(add)
-				break
-			case 's': //subtract
-				calc2(sub)
-				break
-			case 'm': //multiply
-				calc2(mul)
-				break
-			case 'd': //divide
-				calc2(div)
-				break
-			default: null
+		break
+
+		case regexOpr.test(btnID) : {
+			dataEntryMode === true ? (stack.push(format(Number(register))),dataEntryMode = false) : null 
+			switch(btn){
+				case 'g': //x<>y
+					stack.swapxy()
+					register = stack.peek(0)
+					break
+				case 'a': //add
+					calc2(add)
+					break
+				case 's': //subtract
+					calc2(sub)
+					break
+				case 'm': //multiply
+					calc2(mul)
+					break
+				case 'd': //divide
+					calc2(div)
+					break
+				default: null
+			}
+		
 		}
-	}else if (regexCmd.test(btnID)) {	
-		dataEntryMode = false		
-		switch(btn){
-			case 'o': //rolldown				
-				stack.rolldown()
-				register = stack.peek(0)
-				break
-			case 'f': //clear all				
-				stack.clear()
-				register = stack.peek(0)				
-				break
-			case 'r': //return
-				register.length !== 0 ? stack.push(register = (format(Number(register)))) : null
-				break
-			default: null
-		}	
-	}else if (regexSpl.test(btnID)&&register.length!==0){
-		switch(btn){
-			case 'c':
-				dataEntryMode === false ?
-					(register = stack.pop(), stack.push(register = format((register * -1))))	:
-					register.length !== 0 && register != 0 ? register = (register * -1).toString(): null			
-				break
-			case 'b': //backspace // can't go below zero otherwise remove char if only char is negative sign clear
-				dataEntryMode  === false ?  (stack.rolldown(), register = stack.peek(0)) : 
-					register.length !== 0 ? (register = register.substring(0,register.length-1),
-					register.length === 1 && register[0] === '-' ?  register = '' : null) : null 					
-				break
-			default: null
+		break
+
+		case regexCmd.test(btnID) : {	
+			dataEntryMode = false		
+			switch(btn){
+				case 'o': //rolldown				
+					stack.rolldown()
+					register = stack.peek(0)
+					break
+				case 'f': //clear all				
+					stack.clear()
+					register = stack.peek(0)				
+					break
+				case 'r': //return
+					register.length !== 0 ? stack.push(register = (format(Number(register)))) : null
+					break
+				default: null
+			}
+		
 		}
+		break
+
+		case regexSpl.test(btnID)&&register.length!==0 : {
+			switch(btn){
+				case 'c':
+					dataEntryMode === false ?
+						(register = stack.pop(), stack.push(register = format((register * -1))))	:
+						register.length !== 0 && register != 0 ? register = (register * -1).toString(): null			
+					break
+				case 'b': //backspace // can't go below zero otherwise remove char if only char is negative sign clear
+					dataEntryMode  === false ?  (stack.rolldown(), register = stack.peek(0)) : 
+						register.length !== 0 ? (register = register.substring(0,register.length-1),
+						register.length === 1 && register[0] === '-' ?  register = '' : null) : null 					
+					break
+				default: null
+			}
+			break
+		}
+		default : null
 	}
+
 	displayNumber(register.length === 0 ? (format(Number(0))) : register)
 	stack.print()
 }
