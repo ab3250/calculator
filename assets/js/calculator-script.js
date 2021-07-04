2cf5fa5fec84f4377225cdbd4ba900226ad554ad
AB <ab3250@live.com>
2021-07-03 21:03:45 -0400

    2021_07_03_09_03_PM

--- assets/js/calculator-script.js
+++ assets/js/calculator-script.js
@@ -1,37 +1,120 @@
+/* eslint-disable multiline-ternary */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict'
 
-let dataEntryMode = false 
+let dataEntryMode = false
 let register = ''
-const regexDig=/^dig([0-9]{1}|[p])$/ // digits 0-9 + p = period
-const regexOpr=/^opr([a]|[s]|[d]|[m]|[e]|[x]|[g])$/ //operators Add Subtract Divide Multiply  e=sum x=undefined g = x<>y 
-const regexCmd=/^cmd([o]|[r]|[f])$/ // commands mode changing r = return  o=rolldown f=clear
-const regexSpl=/^spl([c]|[b])$/ //non-mode changing commands backspace chs
+const regexDig = /^dig([0-9]{1}|[p])$/ // digits 0-9 + p = period
+const regexOpr = /^opr([a]|[s]|[d]|[m]|[e]|[x]|[g])$/ // operators Add Subtract Divide Multiply  e=sum x=undefined g = x<>y
+const regexCmd = /^cmd([o]|[r]|[f])$/ // commands mode changing r = return  o=rolldown f=clear
+const regexSpl = /^spl([c]|[b])$/ // non-mode changing commands backspace chs
 
 function buttonPress (btnID) {
-	
-	let btn = btnID[3]
+  const btn = btnID[3]
 
-	switch(true){
-		case regexDig.test(btnID) : 
-			processDigits(btn)
-			break
+  switch (true) {
+    case regexDig.test(btnID) :
+      processDigits(btn)
+      break
 
-		case regexOpr.test(btnID) :
-			processOperator(btn)
-			break
+    case regexOpr.test(btnID) :
+      processOperator(btn)
+      break
 
-		case regexCmd.test(btnID) : 
-			processCommands(btn)
-			break
+    case regexCmd.test(btnID) :
+      processCommands(btn)
+      break
 
-		case regexSpl.test(btnID)&&register.length!==0 : 
-			processSpecial(btn)
-			break
+    case regexSpl.test(btnID) && register.length !== 0 :
+      processSpecial(btn)
+      break
+  }
 
-		default : null
-	}
+  displayNumber(register.length === 0 ? (format(Number(0))) : register)
 
-	displayNumber(register.length === 0 ? (format(Number(0))) : register)
-	stack.print()
+  stack.print()
 }
 
+function processDigits (btn) {
+  // eslint-disable-next-line no-unused-expressions
+  dataEntryMode === false ? (register = '', dataEntryMode = true) : null
+  switch (btn) {
+    case 'p':
+      // eslint-disable-next-line no-unused-expressions
+      register.includes('.') ? null : register.length === 0 ? register += '0.' : register += '.'
+      break
+    case '0':
+      register.length === 0 ? register += '' : register += '0'
+      break
+    default: register += btn
+  }
+}
+
+function processCommands (btn) {
+  dataEntryMode = false
+  switch (btn) {
+    case 'o': // rolldown
+      stack.rolldown()
+      register = stack.peek(0)
+      break
+
+    case 'f': // clear all
+      stack.clear()
+      register = stack.peek(0)
+      break
+
+    case 'r': // return
+      register.length !== 0 ? stack.push(register = (format(Number(register)))) : null
+      break
+
+    default: null
+  }
+}
+
+function processOperator (btn) {
+  dataEntryMode === true ? (stack.push(format(Number(register))), dataEntryMode = false) : null
+  switch (btn) {
+    case 'g': // x<>y
+      stack.swapxy()
+      register = stack.peek(0)
+      break
+
+    case 'a': // add
+      calc2(add)
+      break
+
+    case 's': // subtract
+      calc2(sub)
+      break
+
+    case 'm': // multiply
+      calc2(mul)
+      break
+
+    case 'd': // divide
+      calc2(div)
+      break
+
+    default: null
+  }
+}
+
+function processSpecial (btn) {
+  switch (btn) {
+    case 'c':
+      dataEntryMode === false
+        ? (register = stack.pop(), stack.push(register = format((register * -1))))
+        : register.length !== 0 && register !== 0 ? register = (register * -1).toString() : null
+      break
+
+    case 'b': // backspace // can't go below zero otherwise remove char if only char is negative sign clear
+      dataEntryMode === false ? (stack.rolldown(), register = stack.peek(0))
+        : register.length !== 0 ? (register = register.substring(0, register.length - 1),
+        register.length === 1 && register[0] === '-' ? register = '' : null) : null
+      break
+
+    default: null
+  }
+}
--- assets/js/stack.js
+++ assets/js/stack.js
@@ -1,45 +1,48 @@
+/* eslint-disable multiline-ternary */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict'
 
-function Stack() {
-   
-   this.dataStore = ['','','','']
-   
-   this.push = function (element) {
-      this.dataStore.unshift(element)
-      this.dataStore.splice(4,1)    
-   }
+function Stack () {
+  this.dataStore = ['', '', '', '']
 
-   this.rolldown = this.pop = function () {
-      let element = this.dataStore.shift()
-      this.dataStore.splice(3, 0, '')
-      return element === '' ? format(Number(0)) : element
-   }
+  this.push = function (element) {
+    this.dataStore.unshift(element)
+    this.dataStore.splice(4, 1)
+  }
 
-   this.peek = function (loc) {
-      return this.dataStore[loc];
-   }
+  this.rolldown = this.pop = function () {
+    const element = this.dataStore.shift()
+    this.dataStore.splice(3, 0, '')
+    return element === '' ? format(Number(0)) : element
+  }
 
-   this.clear = function () {   
-     this.dataStore.fill('')
-   }
+  this.peek = function (loc) {
+    return this.dataStore[loc]
+  }
 
-   this.length = function () {
-      return this.dataStore.length()
-   }
+  this.clear = function () {
+    this.dataStore.fill('')
+  }
 
-   this.swapxy = function(){
-      let  value1 = this.pop(),
-           value2 = this.pop()
-      this.push(value1)
-      this.push(value2)
-    }
-    
-    this.set = function(x,y){
-      this.dataStore[x] = y   
-   }
+  this.length = function () {
+    return this.dataStore.length()
+  }
 
-   this.print = function(){
-     // console.clear()
-      console.log(this.dataStore + '\n')
-   }
+  this.swapxy = function () {
+    const value1 = this.pop()
+    const value2 = this.pop()
+    this.push(value1)
+    this.push(value2)
+  }
+
+  this.set = function (x, y) {
+    this.dataStore[x] = y
+  }
+
+  this.print = function () {
+    // console.clear()
+    console.log(this.dataStore + '\n')
+  }
 }
